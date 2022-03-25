ac4a6717f51229e72e9fa5a6c818e88302a02d11
Sapnadip Baidya <65809505+SapnadipBaidya@users.noreply.github.com>
2022-03-26 01:38:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Touch } from './touchbox';
+function App() {
+  return (
+    <div className="App">
+    <h1>this is the parent block</h1>
+    <Touch className="touchbox"/>
+
+    </div>
+  );
+}
+
+export default App;
